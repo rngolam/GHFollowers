b02666d0a00d5601bf04e099b4abc73bbcc196a9
Richard Ngo-Lam <rngolam@gmail.com>
2025-02-05 02:26:18 -0800

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Richie Ngo-Lam on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
---
+++ GHFollowersTests/GHFollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersTests.swift
+//  GHFollowersTests
+//
+//  Created by Richie Ngo-Lam on 2/5/25.
+//
+
+import Testing
+@testable import GHFollowers
+
+struct GHFollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
