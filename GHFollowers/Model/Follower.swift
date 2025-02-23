59062f30466bcc331023ec627f73214db50c73bf
Richard Ngo-Lam <rngolam@gmail.com>
2025-02-23 15:15:11 -0800

    Create models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Richie Ngo-Lam on 2/23/25.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    let login: String
+    let avatarUrl: String
+}
