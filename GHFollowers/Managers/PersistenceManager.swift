817a5b15f3ca336a070bc25d8c615d0afced6175
Richard Ngo-Lam <rngolam@gmail.com>
2025-02-25 04:01:27 -0800

    Hook up persistence to Follower and Favorites list VCs

--- GHFollowers/Managers/PersistenceManager.swift
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -35,7 +35,7 @@ enum PersistenceManager {
                 case .remove:
                     retrievedFavorites.removeAll { $0.login == favorite.login }
                 }
-                completion(save(favorites: favorites))
+                completion(save(favorites: retrievedFavorites))
             case .failure(let error):
                 completion(error)
             }
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -113,7 +113,27 @@ class FollowerListVC: UIViewController {
     }
     
     @objc func addButtonTapped() {
-        print("Add Button Tapped")
+        showLoadingView()
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self else { return }
+            self.dismissLoadingView()
+            
+            switch result {
+            case .success(let user):
+                let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+                PersistenceManager.updateWith(favorite: favorite, actionType: .add) { [weak self] error in
+                    guard let self else { return }
+                    if let error {
+                        self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+                        return
+                    }
+                    self.presentGFAlertOnMainThread(title: "Success!", message: "You have successfully favorited this user 🎉", buttonTitle: "Hooray!")
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 }
 
