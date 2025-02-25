12e406431e874c0ea07e12e29f2e53eb6c8d90a4
Richard Ngo-Lam <rngolam@gmail.com>
2025-02-25 00:51:45 -0800

    Create user info header

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Richie Ngo-Lam on 2/25/25.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
