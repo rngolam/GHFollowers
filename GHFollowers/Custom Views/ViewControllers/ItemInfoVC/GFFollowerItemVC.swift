c5e593f059b1e3033212dd8a5b76bd761906e205
Richard Ngo-Lam <rngolam@gmail.com>
2025-02-25 02:18:43 -0800

    Create item info VC subclasses

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVC/GFFollowerItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Richie Ngo-Lam on 2/25/25.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoView1.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoView2.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
