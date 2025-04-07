220dd3472e7a46e227843389017eea06f16a06e6
Richard Ngo-Lam <rngolam@gmail.com>
2025-04-07 16:34:51 -0700

    Implement avatar downloading

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Richie Ngo-Lam on 2/23/25.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseUrl = "https://api.github.com/users/"
+    private let baseUrl = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
