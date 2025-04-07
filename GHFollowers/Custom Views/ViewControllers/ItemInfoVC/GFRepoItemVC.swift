efa2d6662511984dcaad3c164c52824258ad3e3d
Richard Ngo-Lam <rngolam@gmail.com>
2025-04-07 16:34:52 -0700

    Create item info VC subclasses

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVC/GFRepoItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Richie Ngo-Lam on 2/25/25.
+//
+
+import Foundation
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoView1.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoView2.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
