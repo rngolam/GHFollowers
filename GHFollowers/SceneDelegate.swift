bfda6b7b8175ac46ced3a9f5e981eed5dcbd643a
Richard Ngo-Lam <rngolam@gmail.com>
2025-04-07 16:34:51 -0700

    Pass data from keyboard to follower list VC

--- GHFollowers/SceneDelegate.swift
+++ GHFollowers/SceneDelegate.swift
@@ -20,6 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.rootViewController = createTabbar()
         window?.makeKeyAndVisible()
+        
+        configureNavigationBar()
     }
     
     func createSearchNC() -> UINavigationController {
@@ -41,6 +43,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         UITabBar.appearance().tintColor = .systemGreen
         tabbar.viewControllers = [createSearchNC(), createFavoritesNC()]
         return tabbar
+    }
+    
+    func configureNavigationBar() {
+        UINavigationBar.appearance().tintColor = .systemGreen
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
